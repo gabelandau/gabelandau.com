10efa262cb1af84fb08202609305438a61a6077f
Gabe Landau <me@gabelandau.com>
2023-10-21 18:45:20 -0400

    fix: ran prettier formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Gabe Landau
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="text-5xl font-black">
+        Hi, I&apos;m <span className="dark:text-sky-400">Gabe Landau.</span>
+      </div>
+      <div className="text-2xl">I&apos;m a full-stack software engineer and web/IT consultant.</div>
     </main>
   );
 }

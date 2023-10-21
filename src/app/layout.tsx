cc452e4e9a133ed19e952b3eb208e49ce38cc998
Gabe Landau <me@gabelandau.com>
2023-10-21 18:24:02 -0400

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Gabe Landau',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${inter.className} dark:bg-slate-950 dark:text-slate-50`}>{children}</body>
+    </html>
+  );
+}

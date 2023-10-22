62e9e0c5e7be704218734d389d6b69195617c5d9
Gabe Landau <me@gabelandau.com>
2023-10-22 13:05:37 -0400

    feat: added homepage information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="text-5xl font-black">
-        Hi, I&apos;m <span className="dark:text-sky-400">Gabe Landau.</span>
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="m-12 flex max-w-[800px] flex-col gap-2">
+        <div className="pb-4 text-5xl font-black">
+          Hi, I&apos;m <span className="text-sky-400">Gabe Landau</span>
+        </div>
+        <div className="text-xl dark:text-slate-400">
+          I&apos;m a front-end focused full-stack software engineer. Currently, I am building
+          accessible, web-based experiences for Trust & Safety teams at{' '}
+          <a href="https://www.indeed.com/" target="_blank" className="underline dark:text-sky-200">
+            Indeed
+          </a>
+          .
+        </div>
+        <div className="text-xl dark:text-slate-400">
+          I also provide web and IT consulting services through my company,{' '}
+          <a
+            href="https://www.landauwebs.com/"
+            target="_blank"
+            className="underline dark:text-sky-200"
+          >
+            Landau Webs
+          </a>
+          .
+        </div>
       </div>
-      <div className="text-2xl">I&apos;m a full-stack software engineer and web/IT consultant.</div>
     </main>
   );
 }
